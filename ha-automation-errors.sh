dd5f5d33830ba89b432a85d54556ba0ba5d45dc1
Shlomo Dubrowin <shlomo@dubrowin.org>
2025-04-17 09:44:32 +0300

    Add files via upload
    Updated output to be 1 line per event
    Also added a record log file and the ability to not email about the same event twice.

--- ha-automation-errors.sh
+++ ha-automation-errors.sh
@@ -18,13 +18,16 @@ TMP1="/tmp/$( basename "$0" ).1.tmp"
 echo -e "\c" > $TMP1
 TMP2="/tmp/$( basename "$0" ).2.tmp"
 echo -e "\c" > $TMP2
+TMP3="/tmp/$( basename "$0" ).3.tmp"
+echo -e "\c" > $TMP3
+SENDLOG="/var/spool/$( basename "$0" ).log"
+COUNT=0
 
 BACKDIR="/tmp/ha-traces/"
 REMOTEDIR="/mnt/data/supervisor/homeassistant/.storage/"
 HOST="homeassistant"
 RANGE=40
-TO="your_email_here@foo.com"
-FROM="your_email_here@foo.com"
+TO="dubrowin@gmail.com"
 
 mkdir -p $BACKDIR
 
@@ -47,9 +50,9 @@ function Debug {
 
 function EMail {
         Debug "sending via ssmtp"
-        echo -e "To: $TO\nFrom: $FROM\nSubject: `hostname` HA Errors" > $TMP2
+        echo -e "To: $TO\nFrom: shlomo@dubrowin.org\nSubject: `hostname` HA Errors" > $TMP2
 		echo "" >> $TMP2
-		cat $TMP1 >> $TMP2
+		cat $TMP3 >> $TMP2
                 ssmtp $TO < $TMP2 
 }
 
@@ -75,11 +78,28 @@ if [ "$FAILCOUNT" != "0" ]; then
 	#for START in $( grep -n script_execution ${BACKDIR}trace.saved_traces | grep -vi finished | cut -d : -f 1 ); do
 	for START in $( grep -n script_execution ${BACKDIR}trace.saved_traces | grep -vi "finished\|failed_conditions" | cut -d : -f 1 ); do
 		let "END = $START + $RANGE"
-		sed -n -e "$START,${END}p" ${BACKDIR}trace.saved_traces | grep "script_execution\|friendly_name\|last_triggered" | grep -vi "finished" >> $TMP1
+		sed -n -e "$START,${END}p" ${BACKDIR}trace.saved_traces | grep "script_execution\|friendly_name\|last_triggered" | grep -vi "finished" | awk '{$1=$1};1' | sed  's/[ \t]*$//' | sed ':a;N;$!ba;s/,\n/ /g' >> $TMP1
 	done
 
-	Debug "Emailing Report"
-	EMail
+	# Check if the log was already sent
+	while read LINE; do
+		Debug "Checking $LINE"
+		SENT=`grep -c "$LINE" $SENDLOG`
+		if [ "$SENT" != "0" ]; then
+			Debug "LOG entry already sent"
+		else
+			let "COUNT = $COUNT + 1"
+			echo "$LINE" >> $TMP3
+			echo "$LINE" >> $SENDLOG
+		fi
+	done < $TMP1
+
+	if [ "$COUNT" != "0" ]; then
+		Debug "Emailing Report"
+		EMail
+	else
+		Debug "COUNT ($COUNT) no email being sent"
+	fi
 fi
 
 Debug "Finished"
